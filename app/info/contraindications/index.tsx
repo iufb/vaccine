6f9af7d3373025f07f289e0ec6687e596b8c992f
iufb <bashirov3ld2@gmail.com>
2024-09-12 14:18:33 +0500

    fix: con ru, kz ,en

---
+++ app/info/contraindications/index.tsx
@@ -0,0 +1,61 @@
+import { Trans, useTranslation } from 'react-i18next';
+import { ScrollView } from 'react-native';
+import { Container, Zero, One, Two } from '~/components/P';
+import { Link } from '~/components/Link';
+import { StackScreen } from '~/components/StackScreen';
+import { Href } from 'expo-router';
+
+const links = {
+  link1: 'info.contraindications.link1',
+  link2: 'info.contraindications.link2',
+  link3: 'info.contraindications.link3',
+  link4: 'info.contraindications.link4',
+  link5: 'info.contraindications.link5',
+  link6: 'info.contraindications.link6',
+  link7: 'info.contraindications.link7',
+  link8: 'info.contraindications.link8',
+  link9: 'info.contraindications.link9',
+  link10: 'info.contraindications.link10',
+  link11: 'info.contraindications.link11',
+};
+const getLinks = () => {
+  const { t } = useTranslation();
+  const linksObject: Record<string, JSX.Element> = {};
+  Object.keys(links).forEach((l, idx) => {
+    const link = links[l as keyof typeof links];
+    linksObject[`link${idx + 1}`] = (
+      <Link
+        style={{ borderBottomColor: 'gray', borderBottomWidth: 1, marginBottom: 10 }}
+        href={`/info/contraindications/link/${link}` as Href<string>}
+        name={t(`${link}.link`)}></Link>
+    );
+  });
+  return linksObject;
+};
+export default function Contraindications() {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <StackScreen title={t('info.contraindications.title')} />
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 5,
+          paddingVertical: 10,
+          backgroundColor: 'white',
+        }}>
+        <Trans
+          i18nKey={`info.contraindications.data`}
+          components={{
+            container: <Container></Container>,
+            ...getLinks(),
+            zero: <Zero></Zero>,
+            one: <One></One>,
+            two: <Two></Two>,
+            i: <One></One>,
+          }}
+        />
+      </ScrollView>
+    </>
+  );
+}
---
+++ app/info/contraindications/link/[id].tsx
@@ -0,0 +1,40 @@
+import { useGlobalSearchParams } from 'expo-router';
+import { Trans, useTranslation } from 'react-i18next';
+import { ScrollView } from 'react-native';
+import { Zero, One, Two, Container } from '~/components/P';
+import { StackScreen } from '~/components/StackScreen';
+
+export default function ConLink() {
+  const { t } = useTranslation();
+  const params = useGlobalSearchParams();
+
+  return (
+    <>
+      <StackScreen title={t(params.id + '.link')} />
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: 5,
+          marginTop: 10,
+          paddingVertical: 20,
+          backgroundColor: 'white',
+        }}>
+        <Container>
+          <Trans
+            i18nKey={params.id + '.content'}
+            components={{
+              zero: <Zero></Zero>,
+              one: <One></One>,
+              two: <Two></Two>,
+              i: (
+                <One
+                  style={{
+                    fontStyle: 'italic',
+                  }}></One>
+              ),
+            }}
+          />
+        </Container>
+      </ScrollView>
+    </>
+  );
+}
