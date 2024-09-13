0d26b3828a5869d9b506d87837c952d5c854bf3e
iufb <bashirov3ld2@gmail.com>
2024-09-13 11:22:10 +0500

    feat: about fix: kz time

---
+++ app/info/about.tsx
@@ -0,0 +1,35 @@
+import { Trans, useTranslation } from 'react-i18next';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Container, One, P, Zero } from '~/components/P';
+import { StackScreen } from '~/components/StackScreen';
+import { isTablet } from '~/components/constants';
+
+export default function About() {
+  const { t } = useTranslation('');
+  return (
+    <>
+      <StackScreen title={t('info.about.title')} />
+      <SafeAreaView
+        style={{
+          paddingHorizontal: 5,
+          paddingTop: 40,
+          paddingBottom: 10,
+          gap: 10,
+          flex: 1,
+          backgroundColor: 'white',
+        }}>
+        <One style={{ fontSize: isTablet ? 30 : 25, marginBottom: 30, color: '#24C7C7' }}>
+          {t('info.about.name')}
+        </One>
+        <Trans
+          i18nKey={`info.about.data`}
+          components={{
+            container: <Container></Container>,
+            zero: <Zero></Zero>,
+            b: <Zero style={{ fontFamily: 'Rubik_600SemiBold' }}></Zero>,
+          }}
+        />
+      </SafeAreaView>
+    </>
+  );
+}
