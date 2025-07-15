a2ed5043c3f35dd672ef629d9b18353666e993f6
iufb <bashirov3ld2@gmail.com>
2025-07-15 22:55:17 +0500

    fix:revert accordion ; bundleIdentifier

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -1,103 +1,133 @@
 import { FontAwesome } from '@expo/vector-icons';
-
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode } from 'react';
 import {
-  SafeAreaView,
-  StyleProp,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  ViewStyle,
+    SafeAreaView,
+    StyleProp,
+    StyleSheet,
+    TouchableOpacity,
+    View,
+    ViewStyle
 } from 'react-native';
+import Animated, {
+    SharedValue,
+    useAnimatedStyle,
+    useDerivedValue,
+    useSharedValue,
+    withTiming
+} from 'react-native-reanimated';
 import { UIText } from '~/components/P';
-import { isTablet } from '~/components/constants';
 
 export const Accordion = ({ title, children }: { title: string; children: ReactNode }) => {
-  const [open, setOpen] = useState(false);
-  const onPress = () => {
-    setOpen(!open);
-  };
+    const open = useSharedValue(false);
+    const onPress = () => {
+        open.value = !open.value;
+        rotation.value = withTiming(open.value ? 0 : 180, { duration: 400 });
+    };
+    const rotation = useSharedValue(0); // Rotation value in degrees
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
-          <UIText style={styles.btnText}>{title}</UIText>
-          <View>
-            <FontAwesome size={22} color={'white'} name="chevron-down" />
-          </View>
-        </TouchableOpacity>
-      </View>
-      {open && <View style={styles.content}>{children}</View>}
-    </SafeAreaView>
-  );
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ rotate: `${rotation.value}deg` }],
+        };
+    });
+    return (
+        <SafeAreaView style={styles.container}>
+            <View>
+                <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
+                    <UIText style={styles.btnText}>{title}</UIText>
+                    <Animated.View style={[animatedStyle]}>
+                        <FontAwesome size={22} color={'white'} name="chevron-down" />
+                    </Animated.View>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.content}>
+                <AccordionItem isExpanded={open} viewKey="Accordion">
+                    {children}
+                </AccordionItem>
+            </View>
+        </SafeAreaView>
+    );
 };
 function AccordionItem({
-  isExpanded,
-  children,
-  viewKey,
-  style,
+    isExpanded,
+    children,
+    viewKey,
+    style,
+    duration = 200,
 }: {
-  isExpanded: boolean;
-  children: ReactNode;
-  viewKey: string;
-  style?: StyleProp<ViewStyle>;
-  duration?: number;
+    isExpanded: SharedValue<boolean>;
+    children: ReactNode;
+    viewKey: string;
+    style?: StyleProp<ViewStyle>;
+    duration?: number;
 }) {
-  return (
-    <View style={[style]}>
-      <View style={styles.wrapper}>{children}</View>
-    </View>
-  );
+    const height = useSharedValue(0);
+
+    const derivedHeight = useDerivedValue(() =>
+        withTiming(height.value * Number(isExpanded.value), {
+            duration,
+        })
+    );
+    const bodyStyle = useAnimatedStyle(() => ({
+        height: derivedHeight.value,
+        overflow: 'hidden',
+    }));
+
+    return (
+        <Animated.View key={`accordionItem_${viewKey}`} style={[styles.animatedView, bodyStyle, style]}>
+            <View
+                onLayout={(e) => {
+                    height.value = Math.ceil(e.nativeEvent.layout.height);
+                }}
+                style={styles.wrapper}>
+                {children}
+            </View>
+        </Animated.View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    gap: 10,
-    marginBottom: 10,
-    paddingHorizontal: isTablet ? 50 : 20,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    backgroundColor: '#15803d',
-  },
-  btnText: {
-    textAlign: 'left',
-    color: 'white',
-    fontWeight: '900',
-    fontFamily: 'Rubik_400Regular',
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 20,
-  },
-  parent: {
-    width: 200,
-  },
-  wrapper: {
-    position: 'absolute',
-    display: 'flex',
-    borderRadius: 10,
-    paddingVertical: 10,
-    left: 10,
-    right: 10,
-    alignItems: 'center',
-    backgroundColor: '#e2e8f0',
-  },
-  animatedView: {
-    width: '100%',
-    justifyContent: 'center',
-    overflow: 'hidden',
-  },
-  containerBtn: {
-    marginHorizontal: 10,
-  },
+    container: {
+        flex: 1,
+        display: 'flex',
+        gap: 10,
+        marginBottom: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        backgroundColor: '#15803d',
+    },
+    btnText: {
+        textAlign: 'left',
+        color: 'white',
+        fontWeight: '900',
+        fontFamily: 'Rubik_400Regular',
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    parent: {
+        width: 200,
+    },
+    wrapper: {
+        position: 'absolute',
+        display: 'flex',
+        borderRadius: 10,
+        padding: 10,
+        left: 10,
+        right: 10,
+        alignItems: 'center',
+        backgroundColor: '#e2e8f0',
+    },
+    animatedView: {
+        width: '100%',
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
 });
